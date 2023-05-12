ae5775051bb753f767663a6f89f53e1cb52933d3
liyuleizhang <liyuleizhang@163.com>
2023-05-12 20:14:45 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -20,5 +20,5 @@
 #echo 'src-git small https://github.com/kenzok8/small' >>feeds.conf.default
 
 # LEDE
-#echo 'src-git small8 https://github.com/kenzok8/small-package' >>feeds.conf.default
-#echo 'src-git small https://github.com/kenzok8/small' >>feeds.conf.default
+echo 'src-git small8 https://github.com/kenzok8/small-package' >>feeds.conf.default
+echo 'src-git small https://github.com/kenzok8/small' >>feeds.conf.default
